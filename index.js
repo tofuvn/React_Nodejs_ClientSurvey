2300187cc379f9d4c9bfcf1ce92fa9382698b21a
The Ho <thehovnpl@gmail.com>
2019-12-09 04:31:48 +0100

    Setup Heroku

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Welcome");
 })
