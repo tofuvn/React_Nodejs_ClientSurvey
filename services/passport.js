70ec6aec10f06b5eefab0e62dddd82b339cd31fe
The Ho <thehovnpl@gmail.com>
2019-12-10 20:36:22 +0100

    Tweak google strategy setting

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,8 @@ passport.deserializeUser( async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientId,
   clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
+  callbackURL: '/auth/google/callback',
+  proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     const user = await User.findOne({ googleId: profile.id });
     if (user) {
