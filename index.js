33f02fe31792408dd05cb0344e67c5b826994b01
The Ho <thehovnpl@gmail.com>
2019-12-12 10:44:09 +0100

    Config production

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ const PORT = process.env.PORT || 5000;
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Expree will server up the index.html file
+  // if it doesnt recognize the route
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+
 app.listen(PORT, () => {
   console.log("Welcome");
 })
